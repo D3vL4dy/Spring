12413ccf5bb65f48160e60a7cb868200c1452ce1
D3vL4dy <kangji1218@kakao.com>
2022-06-22 11:48:35 +0900

    spring_error

---
+++ spring_error/src/main/java/com/spring/error/HomeController.java
@@ -0,0 +1,25 @@
+package com.spring.error;
+
+import java.sql.SQLException;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) throws Exception{
+		
+		if(true) throw new SQLException("by Controller");
+		
+		return "home";
+	}
+	
+}
